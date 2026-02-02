2c29cc78a3224b7887278be8dfadb0bea7cc340d
nicholashirsch <nicholashirsch@ufl.edu>
2026-02-01 19:30:42 -0500

    bug: changed hohmannpy import method

--- hohmannpy/dynamics/dcms.py
+++ hohmannpy/dynamics/dcms.py
@@ -4,27 +4,26 @@ import numpy as np
 
 
 def euler_2_dcm(angle, axis):
-    match axis:
-        case 1:
+    if axis == 1:
             dcm = np.array(
                 [[1, 0, 0],
                 [0, np.cos(angle), np.sin(angle)],
                 [0, -np.sin(angle), np.cos(angle)]]
             )
             return dcm
-        case 2:
+    elif axis == 2:
             dcm = np.array(
                 [[np.cos(angle), 0, -np.sin(angle)],
                  [0, 1, 0],
                  [np.sin(angle), 0, np.cos(angle)]]
             )
             return dcm
-        case 3:
+    elif axis == 3:
             dcm = np.array(
                 [[np.cos(angle), np.sin(angle), 0],
                  [-np.sin(angle), np.cos(angle), 0],
                  [0, 0, 1]]
             )
             return dcm
-        case _:
-            raise ValueError(f"{axis} is not a valid axis for a Euler angle-based DCM to be generated about.")
+    else:
+        raise ValueError(f"{axis} is not a valid axis for a Euler angle-based DCM to be generated about.")
