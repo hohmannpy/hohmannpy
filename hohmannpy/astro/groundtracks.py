f83d5e706c4327978a3435c503bfec6a9cbe1b68
nicholashirsch <nicholashirsch@ufl.edu>
2026-02-24 10:29:06 -0500

    patch: documentation for Groundtrack

--- hohmannpy/astro/groundtracks.py
+++ hohmannpy/astro/groundtracks.py
@@ -8,14 +8,43 @@ if TYPE_CHECKING:
     from . import spacecraft
 
 
-# TODO: Documentation.
 class Groundtrack:
-    def __init__(self, satellite: spacecraft.Satellite, initial_gmst, solver_tol=1e-8):
+    """
+    The groundtrack of a :class:`~hohmannpy.astro.Satellite` projected onto an ellipsoidal Earth.
+
+    These are stored as geodetic latitude and longitude history arrays. Note that for this class to work, the
+    ``satellite`` must already have had its orbit propagated via :class:`~hohmannpy.astro.Propagator` or one of its
+    child classes. In addition, while the GMST of the Earth is initially computed accurately it is then assumed to
+    evolve linear at the Earth's mean rotation rate (ignoring nutation).
+
+    Parameters
+    ----------
+    satellite : :class:`~hohmannpy.astro.Satellite`
+        Satellite whose ``orbit`` should be used to generate a groundtrack.
+    initial_gmst : float
+        GMST of the Earth at the satellite's initial position.
+    solver_tol : float
+        Root-finding is used to compute the geodetic latitude of the satellite, this is the error tolerance.
+
+    Attributes
+    ----------
+    initial_gmst : float
+        GMST of the Earth at the satellite's initial position.
+    latitude_history : np.ndarray
+        The geodetic latitude of the satellite at each timestep stored in its ``time_history`` attribute.
+    longitude_history : np.ndarray
+        The longitude of the satellite at each timestep stored in its ``time_history`` attribute.
+    """
+
+    def __init__(self, satellite: spacecraft.Satellite, initial_gmst: float, solver_tol: float = 1e-8):
         self.initial_gmst = initial_gmst
         self.latitude_history = np.zeros([1, satellite.time_history.size])
         self.longitude_history = np.zeros([1, satellite.time_history.size])
 
         earth_rot = 7.292115e-5  # Mean rotation rate of the Earth in rad/s.
+
+        # For each discrete time in the satellite's time_history array calculate the corresponding latitude and
+        # longitude using its position.
         for i in range(satellite.time_history.size):
             gmst = self.initial_gmst + earth_rot * satellite.time_history[0, i]
 
@@ -40,5 +69,6 @@ class Groundtrack:
                 )
             self.latitude_history[0, i] = x
 
+            # Calculate the longitude.
             longitude = np.arctan2(position[1], position[0])
             self.longitude_history[0, i] = longitude
