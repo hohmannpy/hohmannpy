patch: fixed body-frame rotation functions

hohmannpy/dynamics/frames.py
@@ -8,7 +8,7 @@ if TYPE_CHECKING:
     from . import quaternions
 
 
-# TODO: Documentation and all functions involving the body frame.
+# TODO: Documentation.
 def eci_2_ecef(vec: np.ndarray, gmst: float) -> np.ndarray:
     return conversions.euler_2_dcm(gmst, 3) @ vec
 
@@ -19,9 +19,10 @@ def eci_2_rtn(vec: np.ndarray, raan: float, inclination: float, argp: float, tru
     return perifocal_2_rtn(eci_2_perifocal(vec, raan, inclination, argp), true_anomaly)
 
 def eci_2_body(
+        vec: np.ndarray,
         quaternion: quaternions.Quaternion,
 ) -> np.ndarray:
-    pass
+    return conversions.quaternion_2_dcm(quaternion) @ vec
 
 def eci_2_heliocentric(vec: np.ndarray) -> np.ndarray:
     earth_tilt = np.deg2rad(-23.439291115)
@@ -47,19 +48,15 @@ def perifocal_2_body(vec: np.ndarray, true_anomaly: float, quaternion: quaternio
 
 def body_2_eci(
         vec: np.ndarray,
-        raan: float,
-        argp: float,
-        inclination: float,
-        true_anomaly: float,
         quaternion: quaternions.Quaternion,
 ) -> np.ndarray:
-    pass
+    return conversions.euler_2_dcm(quaternion).T @ vec
 
-def body_2_perifocal(vec, quaternion: quaternions.Quaternion, true_anomaly: float) -> np.ndarray:
-    pass
+def body_2_perifocal(vec, quaternion: quaternions.Quaternion, raan: float, inclination: float, argp: float) -> np.ndarray:
+    return eci_2_perifocal(body_2_eci(vec, quaternion), raan, inclination, argp)
 
-def body_2_rtn(vec: np.ndarray, quaternion: quaternions.Quaternion) -> np.ndarray:
-    pass
+def body_2_rtn(vec: np.ndarray, quaternion: quaternions.Quaternion, raan: float, inclination: float, argp: float, true_anomaly: float) -> np.ndarray:
+    return eci_2_rtn(body_2_eci(vec, quaternion), raan, inclination, argp, true_anomaly)
 
 def rtn_2_eci(vec: np.ndarray, raan: float, inclination: float, argp: float, true_anomaly: float) -> np.ndarray:
     return perifocal_2_eci(rtn_2_perifocal(vec, true_anomaly), raan, inclination, argp)
@@ -67,8 +64,8 @@ def rtn_2_eci(vec: np.ndarray, raan: float, inclination: float, argp: float, tru
 def rtn_2_perifocal(vec: np.ndarray, true_anomaly: float) -> np.ndarray:
     return conversions.euler_2_dcm(true_anomaly, 3).T @ vec
 
-def rtn_2_body(vec: np.ndarray, quaternion: quaternions.Quaternion) -> np.ndarray:
-    pass
+def rtn_2_body(vec: np.ndarray, quaternion: quaternions.Quaternion, raan: float, inclination: float, argp: float, true_anomaly: float) -> np.ndarray:
+    return eci_2_body(rtn_2_eci(vec, raan, inclination, argp, true_anomaly), quaternion)
 
 def heliocentric_2_eci(vec: np.ndarray) -> np.ndarray:
     earth_tilt = np.deg2rad(-23.439291115)

hohmannpy/perturbations/gravity_gradient.py
@@ -9,7 +9,7 @@ if TYPE_CHECKING:
 
 # TODO: This class.
 class GravityGradient(perturbations.Perturbation):
-    def __init__(self):
+    def __init__(self, attitude_only: bool = True):
         super().__init__()
 
     def evaluate(self, time: float, state: np.ndarray, satellite: spacecraft.Satellite) -> np.ndarray: